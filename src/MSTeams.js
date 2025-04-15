44597ffe4f4e83f48d7d040f04c7f874187bd7cf
sylvainletourneau <sylvain.letourneau@magika.ca>
2025-04-15 17:04:01 -0400

    Enhance error handling for Microsoft Teams notifications by allowing a 200 status code as a valid response. This change improves the robustness of the notification sending process.

--- src/MSTeams.js
+++ src/MSTeams.js
@@ -304,7 +304,7 @@ class MSTeams {
     const response = await client.sendRawAdaptiveCard(payload);
     console.log(response);
 
-    if (response?.status !== 202) {
+    if (response?.status !== 202 && response?.status !== 200) {
       throw new Error('Failed to send notification to Microsoft Teams.\n' + 'Response:\n' + JSON.stringify(response, null, 2));
     }
   }
